666139b616e0ac1d31939502f1afed2f3589b659
kamo-eri <ekamo.tech@gmail.com>
2024-11-15 17:49:36 +0900

    ユーザー登録画面とログイン画面の作成

--- src/main/java/com/example/website/TbWebsiteApplication.java
+++ src/main/java/com/example/website/TbWebsiteApplication.java
@@ -2,12 +2,19 @@ package com.example.website;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TbWebsiteApplication {
+public class TbWebsiteApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TbWebsiteApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TbWebsiteApplication.class, args);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TbWebsiteApplication.class);
+    }
 
 }
---
+++ src/main/java/com/example/website/entity/UserInf.java
@@ -0,0 +1,8 @@
+package com.example.website.entity;
+
+public interface UserInf {
+
+    Long getUserId();
+
+    String getUsername();
+}
