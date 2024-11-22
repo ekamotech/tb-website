e5f0838f9e97fb221f6e5b83814ac8c00cbe322c
kamo-eri <ekamo.tech@gmail.com>
2024-11-22 18:19:43 +0900

    イベントを作成できるようにする

--- src/main/java/com/example/website/controller/EventsController.java
+++ src/main/java/com/example/website/controller/EventsController.java
@@ -3,20 +3,70 @@ package com.example.website.controller;
 import java.io.IOException;
 import java.security.Principal;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.website.entity.UserInf;
+import com.example.website.form.EventForm;
+import com.example.website.service.EventService;
 
 @Controller
 public class EventsController {
     
+    private final EventService eventService;
     
+    public EventsController(EventService eventService) {
+        this.eventService = eventService;
+    }
     
     @GetMapping("/events")
     public String index(Principal principal, Model model) throws IOException {
         
         return "events/index";
     }
+    
+    @GetMapping("/groups/{groupId}/events/new")
+    public String newEvent(@AuthenticationPrincipal UserInf userInf, @PathVariable Long groupId, Model model) throws IOException {
+        
+        EventForm form = eventService.createEventForm(userInf.getUserId(), groupId);
+
+        model.addAttribute("form", form);
+        model.addAttribute("groupId", groupId);
+        return "events/new";
+        
+    }
+    
+    @PostMapping("/groups/{groupId}/events/new")
+    public String createEvent(@AuthenticationPrincipal UserInf userInf, @PathVariable Long groupId, @Validated @ModelAttribute EventForm form, BindingResult result,
+            Model model, RedirectAttributes redirAttrs)
+            throws IOException {
+        
+        if (result.hasErrors()) {
+            model.addAttribute("hasMessage", true);
+            model.addAttribute("class", "alert-danger");
+            model.addAttribute("message", "イベント作成に失敗しました。");
+            return "events/new";
+        }
+        
+        eventService.createEvent(userInf.getUserId(), groupId, form);
+        
+        redirAttrs.addFlashAttribute("hasMessage", true);
+        redirAttrs.addFlashAttribute("class", "alert-info");
+        redirAttrs.addFlashAttribute("message", "イベント作成に成功しました。");
+        
+        return "redirect:/groups";
+        
+    }
+    
+    
     
     
 
--- src/main/java/com/example/website/service/EventService.java
+++ src/main/java/com/example/website/service/EventService.java
@@ -1,8 +1,52 @@
 package com.example.website.service;
 
+import java.io.IOException;
+
+import jakarta.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
+
+import com.example.website.entity.Event;
+import com.example.website.entity.Group;
+import com.example.website.entity.User;
+import com.example.website.form.EventForm;
+import com.example.website.repository.EventRepository;
+import com.example.website.repository.GroupRepository;
+import com.example.website.repository.UserRepository;
 
 @Service
 public class EventService {
+    
+    private final UserRepository userRepository;
+    private final GroupRepository groupRepository;
+    private final EventRepository eventRepository;
 
+    public EventService(UserRepository userRepository, GroupRepository groupRepository, EventRepository eventRepository) {
+        this.userRepository = userRepository;
+        this.groupRepository = groupRepository;
+        this.eventRepository = eventRepository;
+    }
+    
+    public EventForm createEventForm(Long userId, Long groupId) {
+        Group group = groupRepository.findById(groupId).orElseThrow(() -> new IllegalArgumentException("指定されたグループは見つかりませんでした"));
+
+        EventForm form = new EventForm();
+        form.setUserId(userId);
+        form.setGroupId(group.getId());
+        return form;
+    }
+    
+    @Transactional
+    public void createEvent(Long userId, Long groupId, EventForm form) throws IOException {
+        User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("User not found"));
+        Group group = groupRepository.findById(groupId).orElseThrow(() -> new IllegalArgumentException("指定されたグループは見つかりませんでした"));
+        
+        Event event = new Event();
+        event.setUser(user);
+        event.setGroup(group);
+        event.setTitle(form.getTitle());
+        event.setDescription(form.getDescription());
+        eventRepository.saveAndFlush(event);
+        
+    }
 }
