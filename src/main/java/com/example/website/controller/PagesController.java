5e1d29a9f82388ee8b08d977407621d7b1ef78dd
kamo-eri <ekamo.tech@gmail.com>
2025-01-12 20:35:36 +0900

    フッター作成

--- src/main/java/com/example/website/controller/PagesController.java
+++ src/main/java/com/example/website/controller/PagesController.java
@@ -1,13 +1,15 @@
 package com.example.website.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PagesController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("isStartPage", true);
         return "pages/index";
     }
 }
