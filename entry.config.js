bdfa2a57413246e13e2663a2b37167fb5a6dbd87
kamo-eri <ekamo.tech@gmail.com>
2025-01-14 18:05:33 +0900

    デザイン修正

--- entry.config.js
+++ entry.config.js
@@ -4,7 +4,11 @@ module.exports = {
   'index': path.resolve(__dirname, 'src/main/resources/js/index.js'),
   'sub': path.resolve(__dirname, 'src/main/resources/js/sub.js'),
   'module': path.resolve(__dirname, 'src/main/resources/scss/module.scss'),
+  'page/start': path.resolve(__dirname, 'src/main/resources/scss/page/start.scss'),
+  'page/group': path.resolve(__dirname, 'src/main/resources/scss/page/group.scss'),
   'page/group-detail': path.resolve(__dirname, 'src/main/resources/scss/page/group-detail.scss'),
+  'page/event': path.resolve(__dirname, 'src/main/resources/scss/page/event.scss'),
   'page/event-detail': path.resolve(__dirname, 'src/main/resources/scss/page/event-detail.scss'),
-  'page/mypage': path.resolve(__dirname, 'src/main/resources/scss/page/mypage.scss'),
+  'page/favorite': path.resolve(__dirname, 'src/main/resources/scss/page/favorite.scss'),
+  'page/calendar': path.resolve(__dirname, 'src/main/resources/scss/page/calendar.scss'),
 };
